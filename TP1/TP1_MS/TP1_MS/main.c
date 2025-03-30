e619df174205d43c83d38d474c0d5c21d51bcd75
ramirocabral <ramiro.cabral@alu.ing.unlp.edu.ar>
2025-03-30 12:12:18 -0300

    code finished

--- TP1/TP1_MS/TP1_MS/main.c
+++ TP1/TP1_MS/TP1_MS/main.c
@@ -2,69 +2,59 @@
  * TP1_MS.c
  *
  * Created: 29/03/2025 04:46:28 p. m.
- * Author : manus
+ * Author : Manuel Savenia, Cabral Ramiro
  */
-#include <avr/io.h>      // Registros del microcontrolador
-#define F_CPU 16000000UL // Defino la frecuencia de oscilador en 8MHz
-#include <util/delay.h>  // Retardos por software
-/* Función main */
-int main(void)
-{
-    /* Setup */
-    DDRD = 0xFF;                // Configuro Puerto D como salida
-    DDRC &= ~(1 << PORTC0);     // Configuro bit0 puerto C como entrada
-    PORTC |= (1 << PORTC0);     // Habilito Pull-Up en bit0 puerto C
-    int state = 1;              // Variable para almacenar el estado del programa
-    int par = 0;                // Variable para almacenar el estado del par/impar
-    uint8_t valor = 0b00000001; // Variable para almacenar el valor de 8 bits (caso 2)
-    /* Loop */
-    while (1)
-    {
-        switch (state)
-        {
-        case 1:
-            if (!par)
-            {
-                PORTD = 0b10101010; // Escribo Port D con patrón de bits
-                _delay_ms(100);     // Delay de 100 ms
-                PORTD = 0x00;       // Escribo Port D con todos 0
-                _delay_ms(100);     // Delay de 100 ms
-                par = 1;            // Cambio el estado de par/impar
-            }
-            else
-            {
-                PORTD = 0b01010101; // Escribo Port D con otro patrón de bits
-                _delay_ms(100);     // Delay de 100 ms
-                PORTD = 0x00;       // Escribo Port D con todos 0
-                _delay_ms(100);     // Delay de 100 ms
-                par = 0;            // Cambio el estado de par/impar
-            }
-            break;
-        case 2:
-            PORTD = valor;           // Escribo Port D con el valor de 8 bits
-            _delay_ms(100);          // Delay de 100 ms
-            PORTD = 0x00;            // Escribo Port D con todos 0
-            _delay_ms(100);          // Delay de 100 ms
-            valor = valor << 1;      // Desplazo el valor a la izquierda
-            if (valor == 0b00000000) // Si el valor es 0, reinicio
-            {
-                valor = 0b00000001; // Reinicio el valor
-            }
+#include <avr/io.h>
+#define F_CPU 16000000UL // Frecuencia de oscilador en 8MHz
+#include <util/delay.h>
+#include <stdint.h>
 
-            break;
-        case 3:
-            
-            break;
+int main(void){
+    /* Setup */
+	// bit en 0: entrada, 1: salida
+    DDRD = 0xFF;                            // Puerto D como salida
+    DDRB &= (1 << PORTB3 | 1 << PORTB4);    // Bit 3 y 4 puerto C como salida
+    DDRC &= ~(1 << PORTC0 | 1 << PORTC1);	// Bit 0 y 1 puerto C como entrada
+    PORTC |= (1 << PORTC0 | 1 << PORTC1);	// Resistencias Pull-Up en bit 0 y 1 puerto C
+    uint8_t state = 1;                      // Variable para almacenar el estado del programa
+    uint8_t first = 1;                      // Variable para almacenar el estado de cada secuencia
+    uint8_t c0_active = 1;                  // Variable para almacenar el estado del pulsador en C0
+	
+    /* Loop */
+    while (1){
+
+        if (c0_active && (PINC & (1 << PINC0))){
+            c0_active = 0;
         }
-        /* Si el botón es presionado, cambio de estado */
-        if (!(PINC & (1 << PINC0)))
-        {
-            state++;
-            if (state > 3)
-            {
-                state = 1; // Reinicio el estado si supera el máximo
-            }
+
+        // si se presiono el pulsador en C0, cambiamos de estado
+        if (!c0_active && !(PINC & (1 << PINC0))){
+            state = (state == 3) ? 1 : state + 1;
+            first = 1;
+            c0_active = 1;
         }
+
+        //pulsador en el pin C1
+        if ((PINC & (1 << PINC1))){
+            PORTB &= ~(1 << PORTB3 | 1 << PORTB4);
+        } else{
+            PORTB = (PORTB & ~(1 << PORTB4 | 1 << PORTB3)) | (state << PORTB3);
+        }
+
+        switch (state){
+            case 1:
+                PORTD = first ? 0b10101010 : ~PORTD;
+                break;
+            case 2:
+                PORTD = (first || PORTD & (1 << PORTD7)) ? 0b00000001 : PORTD << 1;
+                break;
+            case 3:
+                PORTD = first ? 0b00000001 : PORTD ^ 0b10000001;
+                break;
+        }
+        _delay_ms(100);
+
+        first = 0; //ya no estamos en la primera iteracion de la secuencia
     }
     /* Punto de finalización del programa (NO se debe llegar a este lugar) */
     return 0;
